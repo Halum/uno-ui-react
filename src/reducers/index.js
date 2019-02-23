f2d76c99445da502043838db8ceb656e74ff949e
Sajjad Hossain <my.lost.word@gmail.com>
2019-02-23 20:48:33 +0600

    Fix error on reducer not created

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from "redux";
-import gameReducer from './gameReducer';
 import gameInitializeReducer from './initialReduicer';
 
 export default combineReducers({
-  initializer: gameInitializeReducer,
-  game: gameReducer
+  initializer: gameInitializeReducer
 });
