334ad49362f2cc9d8cea8c0564161e723ad11c2d
Sajjad Hossain <my.lost.word@gmail.com>
2019-02-24 20:01:09 +0600

    Add header

--- src/components/header.component.jsx
+++ src/components/header.component.jsx
@@ -1,10 +1,20 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 export class HeaderComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <nav class="navbar navbar-dark bg-dark">
+          <div class="navbar-brand">Halum Uno</div>
+        </nav>
+      </div>
+    );
   }
 };
 
