e8993e2496d6631f62ec50c6c61a5e8232a119b7
Sajjad Hossain <my.lost.word@gmail.com>
2019-02-28 19:21:47 +0600

    Make board full screen and divide right panel into 2

--- src/components/page.component.jsx
+++ src/components/page.component.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import Board from './board.component';
 import Players from './players.component';
+import {Sprite} from 'react-spritesheet';
+import largeSpriteSheet from './../images/spritesheet_uno.png';
 
 const Page = props => (
-  <div className="container">
-    <div className="row">
-      <div className="col-10">
-        <Board></Board>
+  <div class="container-fluid d-flex h-100 flex-column">
+    <div class="row flex-fill">
+      <div class="col-9 bg-success">
+        <Board ></Board>
       </div>
-      <div className="col">
-        <Players></Players>
+      <div class="col-3">
+        <div class="row h-100">
+          <div class="col-12 h-50 pb-3">
+            <Players></Players>
+          </div>
+          <div class="col-12 h-50 pb-3">
+            <div class="card bg-light h-100">
+
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
