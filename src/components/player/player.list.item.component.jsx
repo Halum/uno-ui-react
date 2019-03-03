1c303d59a5a90afb95927df362562b09e2ed7368
Sajjad Hossain <my.lost.word@gmail.com>
2019-03-04 01:47:58 +0600

    Show up/down arrow based on game direciton

--- src/components/player/player.list.item.component.jsx
+++ src/components/player/player.list.item.component.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {ChevronUpIcon, ChevronDownIcon} from 'react-octicons'
 
 class PlayerListItem extends Component {
   constructor(props) {
@@ -7,9 +8,12 @@ class PlayerListItem extends Component {
 
   render() {
     return (
-      <li className={'list-group-item d-flex justify-content-between align-items-center ' 
+      <li className={'list-group-item d-flex justify-content-between' 
           + (this.props.playing ? 'active' : '')}>
-        {this.props.playerName}
+        <div>
+          <span>{this.props.direction > 0 ? <ChevronDownIcon/> : <ChevronUpIcon/>}</span>
+          <span className="pl-3">{this.props.playerName}</span>
+          </div>
         <span className="badge badge-dark badge-pill">{this.props.cardCount}</span>
       </li>
     );
