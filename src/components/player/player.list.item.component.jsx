c961470644b9235c1cfafac38a158d2c55a1b7ec
Sajjad Hossain <my.lost.word@gmail.com>
2019-03-04 02:43:23 +0600

    Show direction arrow only for active player

--- src/components/player/player.list.item.component.jsx
+++ src/components/player/player.list.item.component.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import {ChevronUpIcon, ChevronDownIcon} from 'react-octicons'
+import { connect } from 'react-redux';
+import {ArrowDownIcon, ArrowUpIcon} from 'react-octicons'
 
 class PlayerListItem extends Component {
   constructor(props) {
@@ -8,16 +9,24 @@ class PlayerListItem extends Component {
 
   render() {
     return (
-      <li className={'list-group-item d-flex justify-content-between' 
+      <li className={'list-group-item d-flex justify-content-between align-items-center ' 
           + (this.props.playing ? 'active' : '')}>
-        <div>
-          <span>{this.props.direction > 0 ? <ChevronDownIcon/> : <ChevronUpIcon/>}</span>
-          <span className="pl-3">{this.props.playerName}</span>
-          </div>
+         <div>
+           <span className={this.props.playing ? 'visible' : 'invisible'}>
+            {this.props.game.direction > 0 ? <ArrowDownIcon/> : <ArrowUpIcon/>}
+           </span>
+           <span className="pl-3">{this.props.playerName}</span>
+           </div>
         <span className="badge badge-dark badge-pill">{this.props.cardCount}</span>
       </li>
     );
   }
 };
 
-export default PlayerListItem;
+const mapStoreToProps = store => {
+  return {
+    game: store.initializer.game
+  };
+};
+
+export default connect(mapStoreToProps)(PlayerListItem);
