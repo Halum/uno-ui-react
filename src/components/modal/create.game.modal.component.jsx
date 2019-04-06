a437aef7f4798df05d2a3095a9fce81c5a7c8b71
Sajjad Hossain <my.lost.word@gmail.com>
2019-04-06 14:45:50 +0600

    Make game ID copied alert non dismissable

--- src/components/modal/create.game.modal.component.jsx
+++ src/components/modal/create.game.modal.component.jsx
@@ -43,11 +43,8 @@ class CreateGameModalComponent extends Component {
 
   getCopySuccessMessage() {
     return (
-      <div className="alert alert-info alert-dismissible fade show" role="alert">
+      <div className="alert alert-info" role="alert">
         Game ID copied to clipboard.
-        <button type="button" className="close" data-dismiss="alert" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
       </div>
     )
   }
